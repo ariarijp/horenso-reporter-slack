8a1589e90832c078d36f66590fe0c71d32da6942
ariarijp <takuya.arita@gmail.com>
2016-01-10 01:06:39 +0900

    Add test cases

---
+++ reporter/reporter_test.go
@@ -0,0 +1,37 @@
+package reporter
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/Songmu/horenso"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAttachments(t *testing.T) {
+	f, _ := os.Open("../fixtures/report_exit_0.json")
+	jsonBytes, _ := ioutil.ReadAll(f)
+
+	var r horenso.Report
+	json.Unmarshal(jsonBytes, &r)
+
+	a := GetAttachments(r)
+
+	assert.Equal(t, "horenso Reporter", a[0].Fallback)
+}
+
+func TestGetSlackChatPostMessageOpt(t *testing.T) {
+	f, _ := os.Open("../fixtures/report_exit_0.json")
+	jsonBytes, _ := ioutil.ReadAll(f)
+
+	var r horenso.Report
+	json.Unmarshal(jsonBytes, &r)
+
+	opts := GetSlackChatPostMessageOpt(r)
+
+	assert.Equal(t, "slack.ChatPostMessageOpt", reflect.TypeOf(opts).String())
+	assert.Equal(t, "[]*slack.Attachment", reflect.TypeOf(opts.Attachments).String())
+}
